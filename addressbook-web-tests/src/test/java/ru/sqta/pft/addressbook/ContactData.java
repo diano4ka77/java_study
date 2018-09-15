3683a6cb6677d2e3eca09b6598f81f5c3330f1c2
diano4ka77 <diano4ka.fly@gmail.com>
2018-09-15 17:31:16 +0500

    Создан тест на создание контакта

---
+++ addressbook-web-tests/src/test/java/ru/sqta/pft/addressbook/ContactData.java
@@ -0,0 +1,49 @@
+package ru.sqta.pft.addressbook;
+
+public class ContactData {
+  private final String firstname;
+  private final String middlename;
+  private final String lastname;
+  private final String mobilephone;
+  private final String email;
+  private final String companyname;
+  private final String year;
+
+  public ContactData(String firstname, String middlename, String lastname, String mobilephone, String email, String companyname, String year) {
+    this.firstname = firstname;
+    this.middlename = middlename;
+    this.lastname = lastname;
+    this.mobilephone = mobilephone;
+    this.email = email;
+    this.companyname = companyname;
+    this.year = year;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getMiddlename() {
+    return middlename;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getMobilephone() {
+    return mobilephone;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getCompanyname() {
+    return companyname;
+  }
+
+  public String getYear() {
+    return year;
+  }
+}
