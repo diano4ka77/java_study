efe075e14125541d3a59ed08dda80b7621f5afca
diano4ka77 <diano4ka.fly@gmail.com>
2018-10-04 14:35:52 +0500

    Сравнение двух множеств после модификации группы

--- addressbook-web-tests/src/test/java/ru/sqta/pft/addressbook/tests/GroupModifyTests.java
+++ addressbook-web-tests/src/test/java/ru/sqta/pft/addressbook/tests/GroupModifyTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.sqta.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModifyTests extends TestBase{
@@ -17,11 +18,16 @@ public class GroupModifyTests extends TestBase{
       List<GroupData> before = app.getGroupHelper().getGroupList();
       app.getGroupHelper().selectGroup(before.size() - 1);
       app.getGroupHelper().editSelectedGroup();
-      app.getGroupHelper().fillGroupForm(new GroupData("test1", null, null));
+      GroupData group = new GroupData(before.get(before.size() - 1).getId(), "test1", null, null);
+      app.getGroupHelper().fillGroupForm(group);
       app.getGroupHelper().submitGroupModify();
       app.getGroupHelper().returnToGroupPage();
       List<GroupData> after = app.getGroupHelper().getGroupList();
       Assert.assertEquals(after.size(), before.size());
+
+      before.remove(before.size() - 1);
+      before.add(group);
+      Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
       app.getSessionHelper().logout();
     }
 }
