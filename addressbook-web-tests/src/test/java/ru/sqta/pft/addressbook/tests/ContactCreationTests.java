e22a8f14a5eb083853cdf85c3699eda498567066
diano4ka77 <diano4ka.fly@gmail.com>
2018-10-04 19:47:45 +0500

    Добавлено сравнение списков в тест на создание контакта

--- addressbook-web-tests/src/test/java/ru/sqta/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/sqta/pft/addressbook/tests/ContactCreationTests.java
@@ -1,13 +1,29 @@
 package ru.sqta.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.sqta.pft.addressbook.model.ContactData;
+import ru.sqta.pft.addressbook.model.GroupData;
+
+import java.util.Comparator;
+import java.util.List;
 
 public class ContactCreationTests extends TestBase {
 
   @Test
   public void testContactCreation() throws Exception {
-    app.getContactHelper().createContact((new ContactData("Testname", "Middlenametest", "Lastnametest", "789456123", "test@gmail.com", "Testcompanyname", "1990", null, "test1")));
+    List<ContactData> before = app.getContactHelper().getContactList();
+    ContactData contact = new ContactData("Testname", "Middlenametest", "Lastnametest", "789456123", "test@gmail.com", "Testcompanyname", "1990", null, "test1");
+    app.getContactHelper().createContact(contact);
+    List<ContactData> after = app.getContactHelper().getContactList();
+    Assert.assertEquals(after.size(), before.size() + 1);
+    before.add(contact);
+
+    Comparator<? super ContactData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before, after);
+
     app.getSessionHelper().logout();
   }
 
