7a77e23b45c28cd9aaefff3c4c250e2734802a8d
diano4ka77 <diano4ka.fly@gmail.com>
2018-10-11 16:03:49 +0500

    fluent интерфейс

--- addressbook-web-tests/src/test/java/ru/sqta/pft/addressbook/tests/GroupModifyTests.java
+++ addressbook-web-tests/src/test/java/ru/sqta/pft/addressbook/tests/GroupModifyTests.java
@@ -14,7 +14,7 @@ public class GroupModifyTests extends TestBase {
   public void ensurePreconditions() {
     app.goTo().groupPage();
     if (app.group().list().size() == 0) {
-      app.group().create(new GroupData("test1", null, null));
+      app.group().create(new GroupData().withName("test1"));
     }
   }
 
@@ -22,7 +22,8 @@ public class GroupModifyTests extends TestBase {
   public void testGroupModify() throws Exception {
     List<GroupData> before = app.group().list();
     int index = before.size() - 1;
-    GroupData group = new GroupData(before.get(index).getId(), "test1", null, null);
+    GroupData group = new GroupData()
+            .withId(before.get(index).getId()).withName("test1");
     app.group().modify(index, group);
     List<GroupData> after = app.group().list();
     Assert.assertEquals(after.size(), before.size());
