7736f7ebb2f0e36e8ff09bc3ed2962cac6a7f4c9
diano4ka77 <diano4ka.fly@gmail.com>
2018-10-24 22:12:06 +0500

    Добавлено логирование

--- addressbook-web-tests/src/test/java/ru/sqta/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/sqta/pft/addressbook/tests/TestBase.java
@@ -1,13 +1,24 @@
 package ru.sqta.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.sqta.pft.addressbook.appmanager.ApplicationManager;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.lang.reflect.Array;
+
 import static org.testng.Assert.fail;
 
+
 public class TestBase {
+
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
 
   protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
   private boolean acceptNextAlert = true;
@@ -25,5 +36,14 @@ public class TestBase {
     if (!"".equals(verificationErrorString)) {
       fail(verificationErrorString);
     }
+  }
+
+  @BeforeMethod(alwaysRun = true)
+  public void logTestStart(Method m, Object[] p) {
+    logger.info("Start test - " + m.getName() + " with parametrs " + Arrays.asList(p));
+  }
+  @AfterMethod(alwaysRun = true)
+  public void logTestStop(Method m, Object[] p) {
+    logger.info("Stop test - " + m.getName() + " with parametrs " + Arrays.asList(p));
   }
 }
