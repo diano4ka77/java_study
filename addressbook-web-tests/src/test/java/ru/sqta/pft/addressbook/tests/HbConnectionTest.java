580b6012db131613b0d658869f8a27888d0baa24
diano4ka77 <diano4ka.fly@gmail.com>
2018-10-26 23:43:06 +0500

    ORM

---
+++ addressbook-web-tests/src/test/java/ru/sqta/pft/addressbook/tests/HbConnectionTest.java
@@ -0,0 +1,54 @@
+package ru.sqta.pft.addressbook.tests;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import ru.sqta.pft.addressbook.model.ContactData;
+import ru.sqta.pft.addressbook.model.GroupData;
+
+import java.util.List;
+
+@Test
+public class HbConnectionTest {
+  private static SessionFactory sessionFactory;
+
+  @BeforeClass
+  protected void setUp() throws Exception {
+    try {
+      // Create the SessionFactory from hibernate.cfg.xml
+      sessionFactory = new Configuration().configure().buildSessionFactory();
+    } catch (Throwable ex) {
+      ex.printStackTrace();
+      throw new ExceptionInInitializerError(ex);
+    }
+  }
+
+  @Test
+  public void testHbConnectionGroup() {
+    Session session = sessionFactory.openSession();
+    session.beginTransaction();
+    List<GroupData> result = session.createQuery("from GroupData").list();
+    for (GroupData group : result) {
+      System.out.println(group);
+    }
+    session.getTransaction().commit();
+    session.close();
+  }
+
+  @Test
+  public void testHbConnectionContact() {
+    Session session = sessionFactory.openSession();
+    session.beginTransaction();
+    List<ContactData> result = session.createQuery("from ContactData where deprecated = '0000-00-00'").list();
+    for (ContactData contact : result) {
+      System.out.println(contact);
+    }
+    session.getTransaction().commit();
+    session.close();
+  }
+}
